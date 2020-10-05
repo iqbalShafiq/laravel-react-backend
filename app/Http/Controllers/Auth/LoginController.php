0a57df9e44fdf513e5af730a688eb7dac7eb90ec
iqbalShafiq <iqbal.syafiq.rozaan@gmail.com>
2020-10-06 00:23:56 +0700

    Login API Added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -15,6 +18,25 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::whereEmail($request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ["The credentials are incorrect"]
+            ]);
+        }
+
+        $user->tokens()->delete();
+        $token = $user->createToken('web-token');
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 }
